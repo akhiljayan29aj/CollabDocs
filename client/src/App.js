b8d64274a712644da51323a1003b47f897e7d5cf
akhiljayan29aj <akhiljayan29.aj@gmail.com>
2021-06-12 01:25:15 +0530

    Client side done

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TextEditor from "./components/text-editor/text-editor.component";
+
+function App() {
+  return <TextEditor />;
+}
+
+export default App;
---
+++ client/src/components/text-editor/text-editor.component.jsx
@@ -0,0 +1,33 @@
+import React, {useCallback} from 'react';
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+import './text-editor.styles.scss'
+
+const TOOLBAR_OPTION = [
+    [{header: [1,2,3,4,5,6,false]}],
+    [{font: []}],
+    [{list: "ordered"}, {list: "bullet"}],
+    ["bold","italic","underline"],
+    [{color: []}, {background: []}],
+    [{script: "sub"}, {script: "super"}],
+    [{align: []}],
+    ["image", "blockquote", "code-block"],
+    ["clean"]
+]
+
+const TextEditor =  () => {
+    const WrapperRef = useCallback((wrapper) => {
+        if(wrapper==null) return;
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div');
+        wrapper.append(editor);
+        new Quill(editor, {theme: "snow", modules:{toolbar:TOOLBAR_OPTION}});
+    }, [])
+
+
+    return (
+        <div className="container" ref={WrapperRef}></div>
+    )
+}
+
+export default TextEditor;
